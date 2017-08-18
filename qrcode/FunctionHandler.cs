1b3bf95f7b8bed639d5a155259e094261874b08b
Robbie Page <rob@rorpage.com>
2017-08-18 18:35:31 -0400

    Initial commit

---
+++ qrcode/FunctionHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using QRCoder;
+
+namespace Function
+{
+    public class FunctionHandler
+    {
+        public void Handle(string input)
+        {
+            var qrGenerator = new QRCodeGenerator();
+            var qrCodeData = qrGenerator.CreateQrCode(input, QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new BitmapByteQRCode(qrCodeData);
+            var qrCodeAsBitmapByteArray = qrCode.GetGraphic(20);
+
+            using (Stream outStream = Console.OpenStandardOutput())
+            {
+                outStream.Write(qrCodeAsBitmapByteArray, 0, qrCodeAsBitmapByteArray.Length);
+            }
+        }
+    }
+}
